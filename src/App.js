97ea4a9afb7aab7189f54cde453af15ff9f06e44
Martin <martinstevo1995@gmail.com>
2022-04-04 16:33:29 +0200

    feat: changes, useState and useEffect added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
+import {Chart as ChartJS} from 'chart.js/auto'
+import {Line} from "react-chartjs-2";
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {apiResponse: ""};
-    }
+// class App extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {apiResponse: ""};
+//     }
 
-    callApiFS() {
+function App() {
+
+    const [tsList, setTsList] = useState([]);
+    const [yList, setYList] = useState([]);
+
+    //callApiFS()
+    const callApiFS = async () =>
+    {
         fetch("http://localhost:9000/getdatafs/", {
             method: "get",
             dataType: 'json',
@@ -17,29 +25,34 @@ class App extends React.Component {
             // .then(res => this.setState({apiResponse: res}));
             .then((res) => res.json())
             .then((data) => {
-                var dataList = [];
-                var yList = [];
-                var uList = [];
-                var tsList = [];
-                var tsMqttList = [];
-                var stateList = [];
+                var tempdataList = [];
+                var tempyList = [];
+                var tempuList = [];
+                var temptsList = [];
+                var temptsMqttList = [];
+                var tempstateList = [];
 
                 for (var i = 0; i < data.length; i++) {
                     var item = data[i];
-                    dataList.push(item);
-                    yList.push(item.y);
-                    uList.push(item.u);
-                    tsList.push(item.timestamp);
-                    tsMqttList.push(item.timestamp_mqtt);
-                    stateList.push(item.state);
+                    tempdataList.push(item);
+                    tempyList.push(item.y);
+                    tempuList.push(item.u);
+                    temptsList.push(item.timestamp.value.toString());
+                    temptsMqttList.push(item.timestamp_mqtt.value.toString());
+                    tempstateList.push(item.state);
                 }
                 //console.log(data);
-                this.setState({dataList})
-                this.setState({yList})
-                this.setState({uList})
-                this.setState({tsList})
-                this.setState({tsMqttList})
-                this.setState({stateList})
+
+                // this.setState({dataList})
+                // this.setState({yList})
+                // this.setState({uList})
+                // this.setState({tsList})
+                // this.setState({tsMqttList})
+                // this.setState({stateList})
+
+                setTsList(temptsList);
+                setYList(tempyList);
+
                 // console.log(this.state.dataList);
                 // console.log(this.state.tsList);
                 // console.log(this.state.tsMqttList);
@@ -51,7 +64,9 @@ class App extends React.Component {
 
     }
 
-    callApiMS() {
+    //callApiMS()
+    const callApiMS = async () =>
+    {
         fetch("http://localhost:9000/getdatams/", {
             method: "get",
             dataType: 'json',
@@ -73,59 +88,81 @@ class App extends React.Component {
 
     }
 
-    componentWillMount() {
-        this.callApiFS();
-        this.callApiMS();
-    }
+    // componentWillMount() {
+    //     this.callApiFS();
+    //     this.callApiMS();
+    // }
+    useEffect(() => {
+        callApiFS();
+        //callApiMS();
+    }, []);
 
-    render() {
-        return (
-            <div className="App">
+    //render() {
+    return (
+        <div className="App">
 
-                {/*logo*/}
+            {/*logo*/}
 
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                </header>
+            {/*<header className="App-header">*/}
+            {/*    <img src={logo} className="App-logo" alt="logo"/>*/}
+            {/*</header>*/}
 
-                {/*<div>{this.state.dataList?.map(value =>*/}
-                {/*    <p>{value.y}</p>*/}
-                {/*)}</div>*/}
+            {/*position graph*/}
 
-                {/*table*/}
-                <table className={"tabular"}>
-                    <tbody>
-                    <tr>
-                        <th>Timestamp</th>
-                        {this.state.tsList?.map(value =>
-                            <td>{value.value.toString()}</td>
-                        )}
-                    </tr>
-                    <tr>
-                        <th>Timestamp MQTT</th>
-                        {this.state.tsMqttList?.map(value =>
-                            <td>{value.value.toString()}</td>
-                        )}
-                    </tr>
-                    <tr>
-                        <th>Ball position</th>
-                        {this.state.yList?.map(value =>
-                            <td>{value.toString()}</td>
-                        )}
-                    </tr>
-                    <tr>
-                        <th>Fan operating speed</th>
-                        {this.state.uList?.map(value =>
-                            <td>{value.toString()}</td>
-                        )}
-                    </tr>
-                    </tbody>
-                </table>
-
-
+            <div className={"graph"}>
+                <Line data={{
+                    labels: tsList,
+                    datasets: [
+                        {
+                            label: "First dataset",
+                            data: yList,
+                            fill: true,
+                            lineTension: 0.18,
+                            backgroundColor: "rgba(75,192,192,0.2)",
+                            borderColor: "rgba(75,192,192,1)"
+                        }
+                    ]
+                }} type={"line"}/>
             </div>
-        );
-    }
+
+            {/*<div>{this.state.dataList?.map(value =>*/}
+            {/*    <p>{value.y}</p>*/}
+            {/*)}</div>*/}
+
+            {/*table*/}
+            {/*<table className={"tabular"}>*/}
+            {/*    <tbody>*/}
+            {/*    <tr>*/}
+            {/*        <th>Timestamp</th>*/}
+            {/*        {this.state.tsList?.map(value =>*/}
+            {/*            <td>{value}</td>*/}
+            {/*        )}*/}
+            {/*    </tr>*/}
+            {/*    <tr>*/}
+            {/*        <th>Timestamp MQTT</th>*/}
+            {/*        {this.state.tsMqttList?.map(value =>*/}
+            {/*            <td>{value}</td>*/}
+            {/*        )}*/}
+            {/*    </tr>*/}
+            {/*    <tr>*/}
+            {/*        <th>Ball position</th>*/}
+            {/*        {this.state.yList?.map(value =>*/}
+            {/*            <td>{value.toString()}</td>*/}
+            {/*        )}*/}
+            {/*    </tr>*/}
+            {/*    <tr>*/}
+            {/*        <th>Fan operating speed</th>*/}
+            {/*        {this.state.uList?.map(value =>*/}
+            {/*            <td>{value.toString()}</td>*/}
+            {/*        )}*/}
+            {/*    </tr>*/}
+            {/*    </tbody>*/}
+            {/*</table>*/}
+
+
+        </div>
+    );
+    //}
 }
 
 export default App;
